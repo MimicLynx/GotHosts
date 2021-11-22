56b29eecf0fd27c6a34d4bb4137e56bda847855a
MimicLynx <66270684+MimicLynx@users.noreply.github.com>
2021-11-22 08:37:32 -0500

    Version 1.1
    first public commit

---
+++ gothosts.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Define colors...
+RED=`tput bold && tput setaf 1`
+GREEN=`tput bold && tput setaf 2`
+YELLOW=`tput bold && tput setaf 3`
+BLUE=`tput bold && tput setaf 4`
+NC=`tput sgr0`
+
+function RED(){
+	echo -e "${RED}${1}${NC}"
+}
+function GREEN(){
+	echo -e "${GREEN}${1}${NC}"
+}
+function YELLOW(){
+	echo -e "${YELLOW}${1}${NC}"
+}
+function BLUE(){
+	echo -e "${BLUE}${1}${NC}"
+}
+
+# Testing if root...
+if [ $UID -ne 0 ]
+then
+  RED "════════════════════════════════════════════════════════════════════════════════"
+	RED "[*] You must run this script as root!"
+  RED "════════════════════════════════════════════════════════════════════════════════"
+	exit
+fi
+
+# title
+function _title {
+BLUE "════════════════════════════════════════════════════════════════════════════════"
+BLUE "  ██████╗  ██████╗ ████████╗    ██╗  ██╗ ██████╗ ███████╗████████╗███████╗██████╗"
+BLUE " ██╔════╝ ██╔═══██╗╚══██╔══╝    ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝██╔════╝╚════██╗"
+BLUE " ██║  ███╗██║   ██║   ██║       ███████║██║   ██║███████╗   ██║   ███████╗  ▄███╔╝"
+BLUE " ██║   ██║██║   ██║   ██║       ██╔══██║██║   ██║╚════██║   ██║   ╚════██║  ▀▀══╝"
+BLUE " ╚██████╔╝╚██████╔╝   ██║       ██║  ██║╚██████╔╝███████║   ██║   ███████║  ██╗"
+BLUE "  ╚═════╝  ╚═════╝    ╚═╝       ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚══════╝  ╚═╝"
+BLUE "════════════════════════════════════════════════════════════════════════════════"
+YELLOW "Simple Subnet Host Discovery Script  - V.1.1"
+YELLOW "By Mimic Lynx"
+BLUE "════════════════════════════════════════════════════════════════════════════════"
+}
+
+# defining IP range and setting output filename
+function config {
+	BLUE "════════════════════════════════════════════════════════════════════════════════"
+  BLUE "[*] Please enter IP range (e.g. 192.168.0.1-120, 192.168.0.0/24)" && echo
+  read IP_RANGE && echo
+	BLUE "════════════════════════════════════════════════════════════════════════════════" && echo
+
+  FILENAME="Alive_hosts.txt"
+  TMP_FILE="tmp.txt"
+  touch $FILENAME
+  touch $TMP_FILE
+}
+
+# fping command
+function fping_scans {
+  BLUE "[*] RUNNING FPING SCAN"
+	if [[ $IP_RANGE == *"-"* ]]; then
+		FPING_IP_RANGE="$(awk -F'-' '{print $1}' <<< 172.16.64.1-255) $(awk -F'.' '{print $1"."$2"."$3"."}' <<< $IP_RANGE)$(awk -F'-' '{print $2}' <<< $IP_RANGE)"
+		fping -a -g $FPING_IP_RANGE 2>/dev/null >> $TMP_FILE
+	else
+		fping -a -g $IP_RANGE 2>/dev/null >> $TMP_FILE
+	fi
+  GREEN "[*] fping scan complete!" && echo
+  BLUE "════════════════════════════════════════════════════════════════════════════════" && echo
+}
+
+# nmap commands
+function nmap_scans {
+  BLUE "[*] RUNNING NMAP SCANS"
+  YELLOW "[*] Starting Ping scan"
+  nmap -n -sn -PE $IP_RANGE -oG - | awk '/Up$/{print $2}' >> $TMP_FILE
+  GREEN "[*] Ping scan complete!"
+  YELLOW "[*] Starting SYN Scan"
+  sudo nmap -n -sS $IP_RANGE -oG - | awk '/Up$/{print $2}' >> $TMP_FILE
+  GREEN "[*] SYN scan complete!"
+	YELLOW "[*] Starting ARP Ping scan"
+  nmap -n -PR $IP_RANGE -oG - | awk '/Up$/{print $2}' >> $TMP_FILE
+  GREEN "[*] ARP Ping scan complete!" && echo
+  BLUE "════════════════════════════════════════════════════════════════════════════════" && echo
+}
+
+# sorting the output
+function cleanup {
+  BLUE "[*] DOING SOME CLEANUP AND SORTING"
+  sort $TMP_FILE | uniq > $FILENAME && rm $TMP_FILE
+  GREEN "[*] Output sorting complete!" && echo
+  BLUE "════════════════════════════════════════════════════════════════════════════════"
+	BLUE "[*] List of Hosts got saved to $FILENAME" && echo
+  cat $FILENAME && echo
+  BLUE "════════════════════════════════════════════════════════════════════════════════"
+}
+
+_title
+config
+fping_scans
+nmap_scans
+cleanup
+exit
